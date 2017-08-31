3873519cd946a7ee47811d529c382f2154d4729a
Matthew Wilkes <tonicblue@gmail.com>
2017-08-31 14:27:29 +0100

    Attempting to help angular webpack use handlebars-helpers

--- dist/route-manager.d.ts
+++ dist/route-manager.d.ts
@@ -1,5 +1,27 @@
 /// <reference types="node" />
 import * as url from 'url';
+import 'handlebars-helpers/array';
+import 'handlebars-helpers/collection';
+import 'handlebars-helpers/date';
+import 'handlebars-helpers/html';
+import 'handlebars-helpers/index';
+import 'handlebars-helpers/logging';
+import 'handlebars-helpers/match';
+import 'handlebars-helpers/misc';
+import 'handlebars-helpers/object';
+import 'handlebars-helpers/regex';
+import 'handlebars-helpers/url';
+import 'handlebars-helpers/code';
+import 'handlebars-helpers/comparison';
+import 'handlebars-helpers/fs';
+import 'handlebars-helpers/i18n';
+import 'handlebars-helpers/inflection';
+import 'handlebars-helpers/markdown';
+import 'handlebars-helpers/math';
+import 'handlebars-helpers/number';
+import 'handlebars-helpers/path';
+import 'handlebars-helpers/string';
+import 'handlebars-helpers/utils';
 import { Route, IRoutes } from './route';
 import { RouteMatch } from './route-match';
 export declare class RouteManager {
